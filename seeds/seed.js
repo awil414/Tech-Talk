fb80a1d1067f33a81c794576e024408253064e61
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-11-02 16:03:32 -0500

    adding commentData.json to seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,16 +2,30 @@ const sequelize = require('../config/connection');
 const { User } = require('../models');
 
 const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
 
-const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
+// const seedDatabase = async () => {
+//   await sequelize.sync({ force: true });
 
-  await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
-  });
+//   const users = await User.bulkCreate(userData, {
+//     individualHooks: true,
+//     returning: true,
+//   });
 
-  process.exit(0);
-};
+//   for (const post of postData) {
+//     await post.create({
+//       ...post,
+//     });
+
+//   // for (const comment of commentData) {
+//   //   await comment.create({
+//   //     ...comment,
+//   //   });
+//   }
+  
+
+//   process.exit(0);
+// };
 
 seedDatabase();
