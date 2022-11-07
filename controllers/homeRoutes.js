533724951fc729e251028d408526036d62b3777a
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-11-07 08:50:11 -0600

    adding withAuth to homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,32 +1,27 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
+const withAuth = require('../utils/auth');
 
 // GET all categories & their associated products
 router.get('/', async (req, res) => {
     try {
       const postData =  await Post.findAll( 
         {
-            attributes: [
-                'id',
-                'title',
-                'post_body',
-            ]
-
-        },
-        {
-        include: [{ 
-            model: Comment, 
-            attributes: ['id', 'comment_text', 'post_id', 'user_id'],
-            include: {
+            attributes: ['id','title','post_body'],
+            include: [{ 
+                model: Comment, 
+                attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+                include: { 
+                    model: User, 
+                    attributes: ['user_name']
+                }
+            },
+                {
                 model: User,
                 attributes: ['user_name']
-            },
-            {
-                model: User,
-                attributes: ['username']
-            },
-        ]
-      });
+                },
+            ]
+      })
       res.status(200).json(categoryData);
     } catch (err) {
       res.status(500).json(err);
