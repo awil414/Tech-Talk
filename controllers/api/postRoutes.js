e273048fa3d4986d58672d792e559b5418424ab0
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-11-07 13:19:01 -0600

    adding get all post and one post by id to post Routes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,7 +1,76 @@
 const router = require('express').Router();
-const { Post } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+// GET /post all POSTS & their associated Comments
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const postData = await PostData.findAll({
+      where: {
+        // use session ID
+        user_id: req.session.user_id,
+      },
+      attributes: ['id', 'title', 'post_body', 'created_at'],
+      // order:[
+      //   ['created_at', 'DESC']
+      // ],
+      include: [
+        {
+          model: User,
+          attributes: ['user_name']
+        },
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['user_name'],
+          },
+        },
+      ],
+    });
+
+    res.status(200).json(postData.reverse);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET /post FIND ONE and its associated Comments
+router.get('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await PostData.findOne({
+      where: {
+        // use session ID
+        user_id: req.session.user_id,
+      },
+      attributes: ['id', 'title', 'post_body', 'created_at'],
+      // order:[
+      //   ['created_at', 'DESC']
+      // ],
+      include: [
+        {
+          model: User,
+          attributes: ['user_name']
+        },
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['user_name'],
+          },
+        },
+      ],
+    });
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// POST create new post
 router.post('/', withAuth, async (req, res) => {
   try {
     const newPost = await Post.create({
@@ -15,6 +84,25 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+// UPDATE (PUT) post
+router.put('/:id', withAuth, async (req, res) => {
+  // update post data
+  try {
+  const updatePost = await Post.update({
+    ...req.body.title, 
+    ...req.body.post_body, 
+    user_id: req.session.user_id,
+    id: req.params.id
+    });
+
+    res.status(200).json(updatePost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+  
+
+// DESTROY delete post
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const postData = await Post.destroy({
