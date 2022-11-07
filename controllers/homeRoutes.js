5d93894889cae333fa16fced5b1b452d997a2b2e
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-11-07 09:14:44 -0600

    adding to function to GET all posts and render to homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
-const { User, Post, Comment } = require('../../models');
+const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-// GET all categories & their associated products
+// GET all POSTS & their associated Comments
 router.get('/', async (req, res) => {
     try {
-      const postData =  await Post.findAll( 
+      const postData =  await PostData.findAll( 
         {
-            attributes: ['id','title','post_body'],
+            attributes: ['id','title','post_body', ],
             include: [{ 
                 model: Comment, 
                 attributes: ['id', 'comment_text', 'post_id', 'user_id'],
@@ -21,7 +21,16 @@ router.get('/', async (req, res) => {
                 attributes: ['user_name']
                 },
             ]
-      })
+        });
+
+        // Serialize data for template to read
+        const posts = postData.map((posts) => posts.get({ plain: true }));
+
+        // Pass serialized data with sessions into template
+        res.render('homepage', {
+            posts,
+            logged_in: req.session.logged_in
+        });
       res.status(200).json(categoryData);
     } catch (err) {
       res.status(500).json(err);
