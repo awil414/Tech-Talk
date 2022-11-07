c13b67a9208ee053182302e2a845fa6dc9b934e2
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-11-07 09:43:43 -0600

    adding function to for user to login

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,4 @@
+// const sequelize = require('../config/connection');  ???
 const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
@@ -24,15 +25,23 @@ router.get('/', async (req, res) => {
         });
 
         // Serialize data for template to read
-        const posts = postData.map((posts) => posts.get({ plain: true }));
+        const posts = postData.map(post => post.get({ plain: true })); //plural on post??
 
         // Pass serialized data with sessions into template
         res.render('homepage', {
             posts,
             logged_in: req.session.logged_in
         });
-      res.status(200).json(categoryData);
     } catch (err) {
       res.status(500).json(err);
     }
+  });
+
+  router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+    res.render('login');
   });
