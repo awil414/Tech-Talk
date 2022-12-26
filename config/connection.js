3099135cf2fbc94e4344f25898786a84e8cbf5e6
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-12-26 17:37:01 -0600

    changing connection.js to JAWSDB_URL

--- config/connection.js
+++ config/connection.js
@@ -1,37 +1,24 @@
+// import Sequelize constructor from the library
 const Sequelize = require('sequelize');
+// import dotenv for password security
 require('dotenv').config();
 
-const sequelize = new Sequelize(
+// create connection to our database, pass in MySql info for username/password
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
-      dialect: 'mysql',
+      host: "localhost",
+      dialect: "mysql",
       port: 3306,
     }
   );
-  
-  module.exports = sequelize;
-  
+}
 
-// let sequelize;
-
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       port: 3306
-//     }
-//   );
-// }
-
-// module.exports = sequelize;
-
-
+module.exports = sequelize;
